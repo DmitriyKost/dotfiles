fcf84a5f01fbd2f44f7d9410fd3bf6a467d9dd49
DmitriyKost <dkostritsyn@gmail.com>
2026-06-30 20:51:15 +0300

    chore: updates and fixes

--- .config/fish/conf.d/50-env.fish
+++ .config/fish/conf.d/50-env.fish
@@ -2,6 +2,13 @@ set -gx GOPATH "$HOME/go"
 set -gx GO111MODULE on
 set -gx MANPAGER 'nvim +Man!'
 
+fish_add_path -g /usr/bin /bin /usr/sbin /sbin
+
+if test -d /opt/homebrew/bin
+    fish_add_path -g /opt/homebrew/bin /opt/homebrew/sbin
+else if test -d /usr/local/bin
+    fish_add_path -g /usr/local/bin /usr/local/sbin
+end
 fish_add_path -g "$HOME/.local/bin"
 fish_add_path -g "$HOME/.cargo/bin"
 fish_add_path -g "$HOME/.opencode/bin"
